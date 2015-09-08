69559f0646b6733160a378327864ac1901a35309
Farheen2302 <farheenfnilofer@gmail.com>
2015-09-08 23:30:31 +0530

    Finally implemented

--- nfatodfa.c
+++ nfatodfa.c
@@ -1,14 +1,3 @@
-//
-//  newfile.c
-//  FAr_LP
-//
-//  Created by admin on 01/09/15.
-//  Copyright 2015 __MyCompanyName__. All rights reserved.
-///
-//  Created by admin on 01/09/15.
-//  Copyright 2015 __MyCompanyName__. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,12 +9,15 @@ int main()
     int i=0,k=0,j=0,n=3,m=2,z=0,s=0,t=0,v=0,nrow=0,r=0,hai=0,ektohai=0,val=0;
     char arr[10][10][10],newarr[10][10][10];
     int check[10][10],fill = 0;
-    p=fopen("mynewfile.txt","r");
+    
+    p=fopen("myfilee.txt","r");
+    
     for(i=0;i<10;i++)
     	for(j=0;j<10;j++)
     		check[i][j]=0;
     i=0;
     j=0;
+    printf("Error\n");
     while(!feof(p))
     {
         j=0;
@@ -35,7 +27,6 @@ int main()
             while(1)
             {
                 fscanf(p,"%c",&ch);
-               // printf("%d =>%d=>%c\n",i,j,ch);
                 if(ch == '-')
                     break;
                 if(ch==' ')
@@ -61,7 +52,7 @@ int main()
     printf("Read the file **hopefully no errors**\n");
     k=0;
     n=i-1;
-  //  printf("Nrow=%d  Ncol = %d\n",n,m);	
+   printf("Nrow=%d  Ncol = %d\n",n,m);	
     
    // m=j;
    m++;
@@ -92,7 +83,7 @@ int main()
 					{	
 					//here we need to check if the check matrix contains the state or not
 					val=0;
-					for(t=0;t<m;t++)
+					for(t=0;t<nrow;t++)
 					{
 						k=0;
 						while(arr[i][j][k]!='\0')
@@ -145,11 +136,10 @@ int main()
 					  while(arr[i][j][k]!='\0')
 					  {
 					  	check[r][arr[i][j][k]-48]= 1;//updating check list of new states
-					  	//need to update original matirx
-					  	for(v=0;v<m;v++)
+					  
+					  	for(v=0;v<m;v++)	//need to update original matirx
 					  	{
 					  		strcat(arr[z][v],arr[arr[i][j][k]-48][v]);
-					  		//printf("str=%s\n",arr[z][v]);
 					  	}
 					  	k++;
 					 }
@@ -163,7 +153,8 @@ int main()
 					
 					newarr[i][j][0]=nrow+s+48;
 					newarr[i][j][1]='\0';
-					//printf("i=%d j=%d val = %d\n",i,j,s);
+				
+                    hai=0;
 				}	
 				            
             }
@@ -171,7 +162,7 @@ int main()
             {
             	newarr[i][j][0]=arr[i][j][0];
             	newarr[i][j][1]='\0';
-            	//printf("i=%d j=%d val = %d\n",i,j,s);
+          
             } 
             fill=0;
             val =0;
@@ -182,25 +173,17 @@ int main()
     }
     
     printf("The output:\n");
-    for(i=0;i<z;i++)
+   for(i=0;i<z;i++)
     {
     	for(j=0;j<m;j++)
     	{
-    		printf("%s ",arr[i][j]);
+    		printf("%s ",newarr[i][j]);
     	}
     	printf("\n");
     }
-  /* printf("Check\n");
-     for(i=0;i<=r;i++)
-    {
-    	for(j=0;j<=m;j++)
-    	{
-    		printf("%d ",check[i][j]);
-    	}
-    	printf("\n");
-    }
-    */
+ 
     return 0;
 }
+
 
 
